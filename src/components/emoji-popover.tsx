3804cb12ebf5c010e2e2935fbffba304f0efd473
Yash Singh <dobbyneedsclothes@gmail.com>
2024-11-13 11:26:15 +0530

    Quill and emoji setup

---
+++ src/components/emoji-popover.tsx
@@ -0,0 +1,22 @@
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+  
+  
+
+const EmojiPopover = () => {
+  return (
+    <div>EmojiPopover</div>
+  )
+}
+
+export default EmojiPopover
